913bdfd868089bd0feabacaad4739e698d3133f4
Marcin Kostrzewa <marckostrzewa@gmail.com>
2022-12-27 14:56:54 +0100

    serve proofs

--- server/server.go
+++ server/server.go
@@ -2,13 +2,51 @@ package server
 
 import (
 	"context"
+	"encoding/json"
+	"io"
 	"net/http"
-	"time"
 	"worldcoin/gnark-mbu/logging"
 
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"worldcoin/gnark-mbu/prover"
 )
+
+type Error struct {
+	StatusCode int
+	Code       string
+	Message    string
+}
+
+func malformedBodyError(err error) *Error {
+	return &Error{StatusCode: http.StatusBadRequest, Code: "malformed_body", Message: err.Error()}
+}
+
+func provingError(err error) *Error {
+	return &Error{StatusCode: http.StatusBadRequest, Code: "proving_error", Message: err.Error()}
+}
+
+func unexpectedError(err error) *Error {
+	return &Error{StatusCode: http.StatusInternalServerError, Code: "unexpected_error", Message: err.Error()}
+}
+
+func (error *Error) MarshalJSON() ([]byte, error) {
+	return json.Marshal(map[string]string{
+		"code":    error.Code,
+		"message": error.Message,
+	})
+}
+
+func (error *Error) send(w http.ResponseWriter) {
+	w.WriteHeader(error.StatusCode)
+	jsonBytes, err := error.MarshalJSON()
+	if err != nil {
+		jsonBytes = []byte(`{"code": "unexpected_error", "message": "failed to marshal error"}`)
+	}
+	length, err := w.Write(jsonBytes)
+	if err != nil || length != len(jsonBytes) {
+		logging.Logger().Error().Err(err).Msg("error writing response")
+	}
+}
 
 func Run(ps *prover.ProvingSystem) (chan struct{}, chan struct{}) {
 	stop := make(chan struct{})
@@ -44,16 +82,34 @@ func Run(ps *prover.ProvingSystem) (chan struct{}, chan struct{}) {
 	proverMux := http.NewServeMux()
 
 	proverMux.HandleFunc("/prove", func(w http.ResponseWriter, r *http.Request) {
-		if r.Method != http.MethodGet {
+		if r.Method != http.MethodPost {
 			w.WriteHeader(http.StatusMethodNotAllowed)
 			return
 		}
-
 		logging.Logger().Info().Msg("received prove request")
-		go func() {
-			time.Sleep(10 * time.Second)
-			w.Write([]byte("hello"))
-		}()
+		buf, err := io.ReadAll(r.Body)
+		if err != nil {
+			malformedBodyError(err).send(w)
+			return
+		}
+		var params prover.Parameters
+		err = json.Unmarshal(buf, &params)
+		if err != nil {
+			malformedBodyError(err).send(w)
+			return
+		}
+		proof, err := ps.Prove(&params)
+		if err != nil {
+			provingError(err).send(w)
+			return
+		}
+		responseBytes, err := json.Marshal(&proof)
+		if err != nil {
+			unexpectedError(err).send(w)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
+		_, err = w.Write(responseBytes)
 	})
 
 	proverServer := &http.Server{Addr: ":3000", Handler: proverMux}
