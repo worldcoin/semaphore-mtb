3497407e1ef9531cf509fe1ea06da13a3e9a1479
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-09-01 22:08:04 +0100

    Fixed Lean namespace

--- formal-verification/FormalVerification/SemanticEquivalence.lean
+++ formal-verification/FormalVerification/SemanticEquivalence.lean
@@ -6,12 +6,12 @@ import FormalVerification
 import FormalVerification.Poseidon.Spec
 import FormalVerification.Poseidon.Correctness
 
-open Poseidon2 (F Order)
+open VerifyProof (F Order)
 
 variable [Fact (Nat.Prime Order)]
 
 def poseidon₂ : Hash F 2 := fun a => (Poseidon.perm Constants.x5_254_3 vec![0, a.get 0, a.get 1]).get 0
 
-lemma Poseidon2_uncps (a b : F) (k : F -> Prop) : Poseidon2.Poseidon2 a b k ↔ k (poseidon₂ vec![a, b]) := by
-    simp [Poseidon2.Poseidon2, poseidon₂, poseidon_3_correct, getElem]
+lemma Poseidon2_uncps (a b : F) (k : F -> Prop) : VerifyProof.Poseidon2 a b k ↔ k (poseidon₂ vec![a, b]) := by
+    simp [VerifyProof.Poseidon2, poseidon₂, poseidon_3_correct, getElem]
     rfl
