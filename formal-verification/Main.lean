0dbdc8b6d08ae477bf8ce8f60c909ecb3d1e17a2
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-09-15 16:26:07 +0100

    Lean circuit

--- formal-verification/Main.lean
+++ formal-verification/Main.lean
@@ -11,27 +11,4 @@ open SemaphoreMTB (F Order)
 
 variable [Fact (Nat.Prime Order)]
 
-set_option maxHeartbeats 2000000000
-theorem before_deletion_tree_matches_root (DeletionIndices: Vector F B) (PreRoot: F) (IdComms: Vector F B) (MerkleProofs: Vector (Vector F D) B) (k: F -> Prop) :
-  gDeletionProof DeletionIndices PreRoot IdComms MerkleProofs k →
-  ∃out, recover_binary_zmod' out = DeletionIndices.head ∧ is_vector_binary out ∧
-  MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec out) MerkleProofs.head IdComms.head = PreRoot := by
-  simp [DeletionProof_looped, deletion_rounds_uncps, DeletionLoop]
-  intros
-  apply Exists.intro
-  tauto
-
-set_option maxHeartbeats 2000000000
-theorem after_deletion_items_are_zero (DeletionIndices: Vector F B) (PreRoot: F) (IdComms: Vector F B) (MerkleProofs: Vector (Vector F D) B) :
-  gDeletionProof DeletionIndices PreRoot IdComms MerkleProofs fun post_root => ∃x, recover_binary_zmod' x = DeletionIndices.last ∧ is_vector_binary x ∧
-  MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec x) MerkleProofs.last 0 = post_root := by
-  -- simp [DeletionProof_2_2_3_2_uncps]
-  -- simp [DeletionLoop]
-  
-  -- simp [SemaphoreMTB.DeletionProof_2_2_3_2]
-  -- simp [DeletionRound_uncps]
-
-  sorry
-
-
 def main : IO Unit := pure ()
