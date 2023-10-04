2458b50b8962014a445e64736c446d8b69fb7562
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-10-04 23:42:51 +0100

    Wip

--- formal-verification/Main.lean
+++ formal-verification/Main.lean
@@ -37,6 +37,27 @@ open SemaphoreMTB renaming InsertionProof_2_3_2_2_3 → gInsertionProof
 
 --   sorry
 
+-- def list_to_vec_n (L : List Dir) (n : Nat) : Vector Dir n := ⟨List.takeI n L, List.takeI_length n L⟩
+
+-- def mod_two (inp : Nat) : Dir := match (inp%2) with
+--  | 0 => Dir.left
+--  | 1 => Dir.right
+--  | _ => panic "Unreachable" -- Unreachable
+
+-- def recover_binary_list : Nat → List Dir
+--   | 0 => [Dir.left]
+--   | 1 => [Dir.right]
+--   | x+2 => have : Nat.succ (x / 2) < Nat.succ (Nat.succ x) := by sorry
+--   (recover_binary_list ((x+2)/2)) ++ [mod_two x]
+-- termination_by recover_binary_list x => x
+
+-- def nat_to_dir_vec (idx : Nat) (depth : Nat ): Vector Dir depth :=
+--   list_to_vec_n (List.reverse (recover_binary_list idx)) depth
+
+-- def item_at_nat {depth : Nat} {F: Type} {H: Hash F 2} (t : MerkleTree F H depth) (idx : Nat) : F := 
+--   let p := nat_to_dir_vec idx depth
+--   t.item_at p
+
 -- IdComms not needed because proving that all items are 0 after deletion
 def item_is_zero_or_skip {n} (Tree: MerkleTree F poseidon₂ D) (DeletionIndices: Vector F n) (PostRoot: F) (MerkleProofs: Vector (Vector F D) n) : Prop :=
   match n with
@@ -48,18 +69,14 @@ def item_is_zero_or_skip {n} (Tree: MerkleTree F poseidon₂ D) (DeletionIndices
       | Bit.one => item_is_zero_or_skip Tree DeletionIndices.tail PostRoot MerkleProofs.tail  -- Skip flag set, don't update the root
 
 -- IdComms not needed because proving that all items are 0 before insertion
-theorem after_deletion_all_items_zero (Tree: MerkleTree F poseidon₂ D) (DeletionIndices: Vector F B) (PreRoot: F) (IdComms: Vector F B) (MerkleProofs: Vector (Vector F D) B) (k: F -> Prop) :
-  gDeletionProof DeletionIndices PreRoot IdComms MerkleProofs fun PostRoot => item_is_zero_or_skip Tree DeletionIndices PostRoot MerkleProofs := by
+theorem after_deletion_all_items_zero (Tree: MerkleTree F poseidon₂ D) (DeletionIndices: Vector F B) (IdComms: Vector F B) (MerkleProofs: Vector (Vector F D) B) (k: F -> Prop) :
+  gDeletionProof DeletionIndices Tree.root IdComms MerkleProofs fun PostRoot => Tree.root = PostRoot ∧ item_is_zero_or_skip Tree DeletionIndices PostRoot MerkleProofs := by
     simp [DeletionProof_uncps]
     unfold DeletionLoop
     unfold item_is_zero_or_skip
-
-    -- conv =>
-    --   arg 1;
-    --   intro out;
-    --   . tactic => cases out; rename_i
+    --apply Exists.intro
+    
     sorry
-
 
 def InsertionLoopTree {n} (Tree: MerkleTree F poseidon₂ D) (StartIndex: F) (PreRoot: F) (IdComms: Vector F n) (MerkleProofs: Vector (Vector F D) n) : Prop :=
   match n with
@@ -73,7 +90,7 @@ def item_is_zero {n} (Tree: MerkleTree F poseidon₂ D) (StartIndex: F) (PreRoot
   match n with
   | Nat.zero => True
   | Nat.succ _ =>
-    ∃out: Vector F (D), recover_binary_zmod' out = StartIndex ∧ is_vector_binary out ∧
+    ∃out: Vector F D, recover_binary_zmod' out = StartIndex ∧ is_vector_binary out ∧
     Tree.item_at (Dir.create_dir_vec out).reverse = 0 ∧ item_is_zero Tree (StartIndex + 1) PreRoot MerkleProofs.tail
 
 theorem recover_tail_reverse_equals_recover'
@@ -82,157 +99,80 @@ theorem recover_tail_reverse_equals_recover'
   (ix : Vector Dir depth)
   (proof : Vector F depth)
   (item : F) :
-  MerkleTree.recover_tail H ix proof item = MerkleTree.recover H ix.reverse proof.reverse item := by sorry
+  MerkleTree.recover_tail H ix proof item = MerkleTree.recover H ix.reverse proof.reverse item := by
+  rw [←Vector.ofFn_get (v := ix)]
+  rw [←Vector.ofFn_get (v := proof)]
+  rw [<-MerkleTree.recover_tail_reverse_equals_recover H _ _ item]
+  simp
 
--- theorem proof_ceritfies_item'
---   {depth : Nat}
---   {F: Type}
---   {H: Hash F 2}
---   [Fact (perfect_hash H)]
---   (ix : Vector Dir depth)
---   (tree : MerkleTree F H depth)
---   (proof : Vector F depth)
---   (item : F)
---   :
---   MerkleTree.recover H ix.reverse proof.reverse item = tree.root → tree.item_at ix.reverse = item := by sorry
-
-lemma verify_proof_is_root {Tree: MerkleTree F poseidon₂ D} {Path: Vector Dir D} {Proof: Vector F D} {Item: F} :
-  (MerkleTree.recover_tail poseidon₂ Path Proof Item) = (MerkleTree.recover_tail poseidon₂ Path (Tree.proof Path) (Tree.item_at Path)) := by sorry
-
-
-def TreeInsert [Fact (perfect_hash poseidon₂)] (Index: F) (Item: F) (Tree : MerkleTree F poseidon₂ D) (Proof: Vector F D) (k: F -> Prop) : Prop :=
+def TreeInsert [Fact (perfect_hash poseidon₂)] (Tree : MerkleTree F poseidon₂ D) (Index: F) (Item: F) (Proof: Vector F D) (k: F -> Prop) : Prop :=
   ∃out: Vector F D, recover_binary_zmod' out = Index ∧ is_vector_binary out ∧
   MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec out) Proof 0 = Tree.root ∧
-  k (Tree.set (Dir.create_dir_vec out) Item).root
+  k (Tree.set (Dir.create_dir_vec out).reverse Item).root
 
 theorem InsertIsSet [Fact (perfect_hash poseidon₂)] {Tree : MerkleTree F poseidon₂ D} {Index: F} {Item: F} {Proof: Vector F D} {k: F -> Prop} :
-  insertion_round Index Item () Proof k ↔
-  TreeInsert Index Item PrevRoot Proof k
-  -- := by
-  -- simp [InsertionRound_uncps]
-  -- conv =>
-  --   rhs; arg 1; intro out; rw [<-MerkleTree.recover_proof_is_root poseidon₂ (Dir.create_dir_vec out) _]
-  -- apply exists_congr
-  -- intro out
-  -- simp
-  -- intros
-  -- have : (MerkleTree.proof (MerkleTree.set Tree (Dir.create_dir_vec out) Item) (Dir.create_dir_vec out)) = Proof := by sorry
-  -- simp [this]
-  -- have : (MerkleTree.item_at (MerkleTree.set Tree (Dir.create_dir_vec out) Item) (Dir.create_dir_vec out)) = Item := by sorry
-  -- simp [this]
+  insertion_round Index Item Tree.root Proof k ↔
+  TreeInsert Tree Index Item Proof k := by
+    simp [insertion_round]
+    simp [TreeInsert]
+    apply exists_congr
+    intro out
+    simp
+    intros
+    have : MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec out) Proof Item = (Tree.set (Dir.create_dir_vec out).reverse Item).root := by
+      simp [recover_tail_reverse_equals_recover'] at *
+      rw [MerkleTree.proof_insert_invariant (Dir.create_dir_vec out).reverse Tree 0 Item Proof.reverse]
+      assumption
+    simp [<-MerkleTree.recover_tail_reverse_equals_recover] at this
+    simp [this]
 
-  -- rw [MerkleTree.proof_ceritfies_item (Dir.create_dir_vec out) _ Proof 0]
-  -- sorry
-  -- sorry
+-- theorem nat_to_dir_vec_is_recovery {idx : Nat} {depth : Nat} {out : Vector F depth} { h : recover_binary_zmod' out = idx ∧ is_vector_binary out }:
+--   (nat_to_dir_vec idx depth) = (Dir.create_dir_vec out).reverse := by
+--   cases h
+--   rename_i h₁ h₂
+--   sorry
 
 theorem before_insertion_all_items_zero
   [Fact (perfect_hash poseidon₂)]
   {Tree: MerkleTree F poseidon₂ D}
   (StartIndex: Nat) (IdComms: Vector F B) (MerkleProofs: Vector (Vector F D) B) (k: F -> Prop) :
   gInsertionProof ↑StartIndex Tree.root IdComms MerkleProofs k →
-  (∀ i ∈ [StartIndex:StartIndex + B], Tree.item_at_nat i = 0) := by
-  --item_is_zero Tree StartIndex PreRoot MerkleProofs := by
+  -- (∀ i ∈ [StartIndex:StartIndex + B], item_at_nat Tree i = 0) := by
+  (∀ i ∈ [StartIndex:StartIndex + B], ∃out: Vector F D, recover_binary_zmod' out = i ∧ is_vector_binary out ∧ 
+  (Tree.item_at (Dir.create_dir_vec out).reverse) = 0) := by
     simp [gInsertionProof]
     simp [InsertionRound_uncps]
+
+    -- First version!
+    -- repeat (
+      -- simp only [InsertIsSet]
+      -- unfold TreeInsert
+    -- )
+    -- simp
+    -- simp [item_at_nat]
+    -- simp [recover_tail_reverse_equals_recover']
+    -- intros
+    -- rename_i _ _ h₃ _ _ _ _ _ i hi
+    -- simp [Gates.add] at *
+    -- rw [MerkleTree.proof_ceritfies_item _ _ MerkleProofs[0].reverse _]
+    -- rw [nat_to_dir_vec_is_recovery]
+    -- apply h₃
+    -- rename_i h₈
+    -- simp [Std.instMembershipNatRange] at hi
+
+    -- Second version!
     simp only [InsertIsSet]
-    simp [TreeInsert]
-    simp [item_is_zero]
+    unfold TreeInsert
+    simp
     simp [recover_tail_reverse_equals_recover']
     intros
-    rename_i h₁ h₂ h₃ h₄ h₅ h₆ h₇ h₈ h₉ h₁₀ h₁₁ h₁₂ h₁₃
-    rw [MerkleTree.proof_ceritfies_item] at h₁₂
-    -- apply Exists.intro
-    -- apply And.intro
-    -- rename_i h₁ h₂ h₃ h₄ h₅ h₆ h₇ h₈ h₉ h₁₀ h₁₁ h₁₂ h₁₃
-    -- unfold Gates.add at h₂
-    -- simp at *
-    -- assumption
-    -- apply And.intro
-    -- assumption
-    -- apply And.intro
-    -- rw [MerkleTree.proof_ceritfies_item] at *
-    -- assumption
-    -- rename_i h₁ h₂ h₃ h₄ h₅ h₆ h₇ h₈ h₉ h₁₀ h₁₁ h₁₂ h₁₃
-    -- subst_vars
-    -- --rw [←Vector.ofFn_get (v := MerkleProofs)] at *
-    -- have : h₆ = (Vector.reverse MerkleProofs[0]) ∧ Tree = h₄ := by
-    --   apply And.intro
-
-    --   sorry
-    -- simp [this]
-    -- assumption
+    apply Exists.intro
+    apply And.intro
+    rename_i i hi
     sorry
-    -- apply Exists.intro
-    -- apply And.intro
-    -- unfold Gates.add at *
-    -- assumption
-    -- apply And.intro
-    -- assumption
-    -- simp [recover_tail_reverse_equals_recover'] at *
-    -- rw [MerkleTree.proof_ceritfies_item] at *
-    -- assumption
-    -- simp [*] at *
-
-    -- sorry
-
--- theorem before_insertion_all_items_zero [Fact (perfect_hash poseidon₂)] {Tree: MerkleTree F poseidon₂ D} (StartIndex: F) (PreRoot: F) (IdComms: Vector F B) (MerkleProofs: Vector (Vector F D) B) (k: F -> Prop) :
---   (Tree.root = PreRoot ∧ gInsertionProof StartIndex PreRoot IdComms MerkleProofs fun postRoot => Tree.root = postRoot)  → item_is_zero Tree StartIndex PreRoot MerkleProofs := by
---   simp [InsertionProof_uncps]
---   simp [InsertionLoop]
---   simp [item_is_zero]
---   simp [recover_tail_reverse_equals_recover']
-
-
---   intros
---   subst_vars
---   apply Exists.intro
---   repeat' (apply And.intro)
---   rfl
---   assumption
---   rw [MerkleTree.proof_ceritfies_item]
---   assumption
---   --simp [<-proof_ceritfies_item'] at *
---   -- rename_i h₁ h₂ h₃ h₄ h₅ h₆ h₇ h₈
---   -- let ix := (Dir.create_dir_vec h₁).reverse
---   -- let t := Tree
---   -- let p := (Vector.head MerkleProofs).reverse
---   -- let item := (0:F)
---   -- rw [MerkleTree.proof_ceritfies_item ix t p (0:F)]
---   -- simp
---   -- rw [<-MerkleTree.recover_tail_reverse_equals_recover poseidon₂ ix p (0:F)]
---   -- simp
---   -- simp [h₈]
---   -- --rename_i h₁ h₂ h₃ h₄ h₅ h₆ h₇ h₈
---   -- apply Exists.intro
---   -- repeat' (apply And.intro)
---   -- --assumption
---   -- rename_i h₁ h₂ h₃ h₄ h₅ h₆ h₇ h₈
---   -- --assumption
---   -- --let ix := (Dir.create_dir_vec h₃).reverse
---   -- --let t := Tree
---   -- --let p := (Vector.head (Vector.tail MerkleProofs)).reverse
---   -- --let item := (0:F)
---   -- --rw [MerkleTree.proof_ceritfies_item ix t p (0:F)]
---   -- --simp
---   -- --simp
---   -- --rw [<-MerkleTree.recover_tail_reverse_equals_recover poseidon₂ ix p (0:F)]
---   -- -- simp
---   -- -- have: Tree.root = MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec h₁) (Vector.head MerkleProofs) (Vector.head IdComms) := by
---   -- --   simp [*]
---   -- --   sorry
---   -- -- simp [this]
---   -- -- assumption
---   -- assumption
---   -- assumption
---   -- simp [recover_tail_reverse_equals_recover'] at *
---   -- rw [MerkleTree.proof_ceritfies_item]
---   -- assumption
---   -- rename_i h₁ h₂ h₃ h₄ h₅ h₆ h₇ h₈
-
-
---   --rw [MerkleTree.proof_ceritfies_item _ _ _ _]
---   --rw [<-MerkleTree.recover_tail_reverse_equals_recover _ _ _ _]
---   sorry
---   sorry
+    apply And.intro
+    assumption
+    rw [MerkleTree.proof_ceritfies_item _ _ MerkleProofs[0].reverse _]
+    assumption
 
 def main : IO Unit := pure ()
