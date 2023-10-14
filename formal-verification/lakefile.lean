ec21fa04dc180a25b58f45605b46eca441a2401e
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-10-14 19:44:05 +0100

    Added Lean Args to main

--- formal-verification/lakefile.lean
+++ formal-verification/lakefile.lean
@@ -12,11 +12,12 @@ require ProvenZK from git
   "https://github.com/reilabs/proven-zk.git"@"4901484155bdc733b62ac22a024d77ddb5ff9d6f"
 
 lean_lib FormalVerification {
-  moreLeanArgs := #["--tstack=65520", "-DmaxRecDepth=100000", "-DmaxHeartbeats=200000000"]
+  moreLeanArgs := #["--tstack=65520", "-DmaxRecDepth=10000", "-DmaxHeartbeats=200000000"]
   -- add library configuration options here
 }
 
 @[default_target]
 lean_exe «formal-verification» {
+  moreLeanArgs := #["--tstack=65520", "-DmaxRecDepth=10000", "-DmaxHeartbeats=200000000"]
   root := `Main
 }
