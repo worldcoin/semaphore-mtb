af764b2124de7f670e6d5518b3dd71b05307a4c0
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-10-19 16:55:51 +0100

    Wip

--- formal-verification/Main.lean
+++ formal-verification/Main.lean
@@ -137,4 +137,130 @@ theorem before_insertion_all_items_zero
   apply before_insertion_all_items_zero_loop
   simp [ixBound]
 
+def insertion_circuit [Fact (perfect_hash poseidon₂)] (Tree : MerkleTree F poseidon₂ D) (StartIndex : F) (IdComms: Vector F b) (MerkleProofs: Vector (Vector F D) b) (k : F → Prop): Prop :=
+  match b with
+  | Nat.zero => k Tree.root
+  | Nat.succ _ => MerkleTree.item_at_nat Tree StartIndex.val = some 0 ∧
+                  MerkleTree.proof_at_nat Tree StartIndex.val = some MerkleProofs.head.reverse ∧
+                  ∃postTree, MerkleTree.set_at_nat Tree StartIndex.val IdComms.head = some postTree ∧
+                  insertion_circuit postTree StartIndex.succ IdComms.tail MerkleProofs.tail k
+
+theorem insertion_is_set
+  [Fact (perfect_hash poseidon₂)]
+  {Tree: MerkleTree F poseidon₂ D}
+  (StartIndex: Nat) (IdComms: Vector F b) (MerkleProofs: Vector (Vector F D) b) (PostRoot : F)
+  {ixBound: StartIndex + b < Order}:
+  (InsertionLoop ↑StartIndex Tree.root IdComms MerkleProofs fun newRoot => PostRoot = newRoot) ↔
+  (insertion_circuit Tree ↑StartIndex IdComms MerkleProofs fun newRoot => PostRoot = newRoot) := by
+  apply Iff.intro
+  . induction b generalizing StartIndex Tree with
+    | zero =>
+      simp [InsertionLoop, insertion_circuit]
+    | succ _ ih =>
+      simp only [InsertionLoop, insertion_circuit]
+      rintro ⟨ixbin, _⟩
+      casesm* (_ ∧ _)
+      have : nat_to_bits_le D (↑StartIndex:F).val = some (vector_zmod_to_bit ixbin) := by
+        apply recover_binary_zmod'_to_bits_le
+        . simp
+        . assumption
+        . rename_i h _ _ _; simp[h]
+      rename_i h₀ h₁ h₂ h₃
+      unfold MerkleTree.item_at_nat
+      unfold MerkleTree.proof_at_nat
+      unfold MerkleTree.set_at_nat
+      unfold Dir.nat_to_dir_vec
+      rw [this]
+      simp [←Dir.create_dir_vec_bit]
+      refine ⟨?_, ⟨?_, ?_⟩⟩
+      . apply MerkleTree.proof_ceritfies_item (proof := MerkleProofs.head.reverse)
+        rw [<-MerkleTree.recover_tail_equals_recover_reverse]
+        simp [h₂]
+      . apply MerkleTree.recover_proof_reversible (Item := (0:F))
+        rw [<-MerkleTree.recover_tail_equals_recover_reverse]
+        simp [h₂]
+      . have this : MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec ixbin) (Vector.head MerkleProofs) (Vector.head IdComms) = (MerkleTree.set Tree (Vector.reverse (Dir.create_dir_vec ixbin)) (Vector.head IdComms)).root := by
+          simp [MerkleTree.recover_tail_equals_recover_reverse]
+          apply MerkleTree.proof_insert_invariant (old := (0:F)) (new := IdComms.head)
+          rw [<-MerkleTree.recover_tail_equals_recover_reverse]
+          simp [h₂]
+        simp [this] at h₃
+        
+        --rw [ZMod.int_cast_zmod_cast (a := (↑StartIndex:F))]
+        apply ih
+        simp
+        rw [<-ZMod.val_nat_cast]
+        simp [Nat.succ] at ixBound
+        simp [ZMod.val_cast_of_lt]
+        have this : (↑↑StartIndex + 1:Nat) = (StartIndex + 1:Nat) := by
+          simp [Fin.ext]
+        
+        sorry
+        simp
+        sorry
+  . induction b generalizing StartIndex Tree with
+    | zero =>
+      simp [InsertionLoop, insertion_circuit]
+    | succ _ ih =>
+      simp only [InsertionLoop, insertion_circuit]
+      rintro ⟨hitem, ⟨hproof, ⟨ftree, ⟨hftree, hresult⟩⟩⟩⟩
+      simp [MerkleTree.item_at_nat, Dir.nat_to_dir_vec] at hitem
+      rcases hitem with ⟨bits, ⟨hbits, hitem_at⟩⟩
+      simp [MerkleTree.proof_at_nat, Dir.nat_to_dir_vec] at hproof
+      rcases hproof with ⟨bits', ⟨hbits', hproof_at⟩⟩
+      simp [hbits] at hbits'
+      simp [hbits'] at *
+      simp [MerkleTree.set_at_nat, Dir.nat_to_dir_vec] at hftree
+      rcases hftree with ⟨bits'', ⟨hbits'', hftree_at⟩⟩
+      simp [hbits''] at hbits
+      rw [←Vector.vector_reverse_eq] at hproof_at
+      simp [hbits] at *
+      let t : Vector F D := (Vector.map Bit.toZMod bits')
+      refine ⟨t, ?_⟩
+      refine ⟨?_, ⟨?_, ⟨?_, ?_⟩⟩⟩
+      . apply recover_binary_of_to_bits
+        simp [hbits'']
+      . apply vector_binary_of_bit_to_zmod
+      . simp [<-hproof_at]
+        rw [MerkleTree.recover_tail_equals_recover_reverse, Dir.create_dir_vec_bit, zmod_to_bit_coe, ←hitem_at]
+        simp [MerkleTree.recover_proof_is_root]
+      . 
+        -- (MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec t) (Vector.head MerkleProofs) (Vector.head IdComms))
+        -- needs to be equal to
+        -- ftree == MerkleTree.set Tree (Vector.reverse (Vector.map Dir.bit_to_dir bits')) (Vector.head IdComms)
+
+
+        -- MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec out) MerkleProofs.head 0 = Tree.root
+        -- corresponds to
+        -- MerkleTree.item_at_nat Tree StartIndex.val = some 0 ∧
+        -- MerkleTree.proof_at_nat Tree StartIndex.val = some MerkleProofs.head.reverse
+
+        -- (MerkleTree.recover_tail poseidon₂ (Dir.create_dir_vec out) MerkleProofs.head IdComms.head)
+        -- corresponds to
+        -- MerkleTree.set_at_nat Tree StartIndex.val IdComms.head = some postTree
+        rw [MerkleTree.recover_tail_equals_recover_reverse, Dir.create_dir_vec_bit, zmod_to_bit_coe]
+        rw [MerkleTree.proof_insert_invariant (tree := Tree) (old := (0:F)) (new := IdComms.head)]
+        have this : (↑StartIndex + 1:F) = ↑(StartIndex + 1) := by
+          simp [Fin.ext]
+        rw [this]
+        apply ih
+        linarith only [ixBound]
+        rw [<-hftree_at] at hresult
+        rotate_left
+        simp [<-hproof_at]
+        simp [<-hitem_at]
+        simp [MerkleTree.recover_proof_is_root]
+        sorry
+
+theorem insertion_is_set_circuit
+  [Fact (perfect_hash poseidon₂)]
+  {Tree: MerkleTree F poseidon₂ D}
+  (StartIndex: Nat) (IdComms: Vector F B) (MerkleProofs: Vector (Vector F D) B) (PostRoot : F)
+  {ixBound: StartIndex + B < Order}:
+  (gInsertionProof ↑StartIndex Tree.root IdComms MerkleProofs fun newRoot => PostRoot = newRoot) ↔
+  (insertion_circuit Tree ↑StartIndex IdComms MerkleProofs fun newRoot => PostRoot = newRoot) := by
+  rw [InsertionProof_uncps]
+  apply insertion_is_set
+  simp [ixBound]
+
 def main : IO Unit := pure ()
