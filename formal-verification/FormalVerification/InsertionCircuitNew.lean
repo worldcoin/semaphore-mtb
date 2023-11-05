07358dcf9ec679bd2dffd0c9bb129c77a6716a85
Giuseppe <8973725+Eagle941@users.noreply.github.com>
2023-11-05 21:21:21 +0000

    Wip

--- formal-verification/FormalVerification/InsertionCircuitNew.lean
+++ formal-verification/FormalVerification/InsertionCircuitNew.lean
@@ -18,6 +18,36 @@ set_option pp.coercions false
 
 ------------ MISC
 
+def is_index_in_range {n : Nat} (D : Nat) (a : ZMod n) : Prop :=
+  a.val < 2^D
+
+def are_indices_in_range {d n : Nat} (D : Nat) (a : Vector (ZMod n) d) : Prop :=
+  List.foldr (fun x r => (is_index_in_range D x) ∧ r) True a.toList
+
+theorem are_indices_in_range_cons {d n : Nat} (D : Nat) (a : ZMod n) (vec : Vector (ZMod n) d) :
+  are_indices_in_range D (a ::ᵥ vec) ↔ is_index_in_range D a ∧ are_indices_in_range D vec := by
+  unfold are_indices_in_range
+  conv => lhs; unfold List.foldr; simp
+
+theorem are_indices_in_range_split {d n : Nat} (D : Nat) (a : Vector (ZMod n) (d+1)) :
+  are_indices_in_range D a ↔ is_index_in_range D a.head ∧ are_indices_in_range D a.tail := by
+  rw [<-are_indices_in_range_cons]
+  simp
+
+lemma head_index_in_range {b : Nat} (D : Nat) (Index : Vector (ZMod n) (b+1)) (xs_small : are_indices_in_range D Index) :
+  is_index_in_range D (Vector.head Index) := by
+  rw [are_indices_in_range_split] at xs_small
+  cases xs_small
+  rename_i x xs
+  simp [x]
+
+lemma tail_index_in_range {b : Nat} (D : Nat) (Index : Vector (ZMod n) (b+1)) (xs_small : are_indices_in_range D Index) :
+  are_indices_in_range D (Vector.tail Index) := by
+  rw [are_indices_in_range_split] at xs_small
+  cases xs_small
+  rename_i x xs
+  simp [xs]
+
 lemma proof_of_set
   {F d}
   (H : Hash F 2)
@@ -62,6 +92,15 @@ lemma proof_of_set
       simp [Vector.vector_eq_cons]
       rw [ih]
 
+def tree_item_at_fin {F: Type} {H: Hash F 2} (Tree : MerkleTree F H d) (i : Fin (2^d)): F :=
+  MerkleTree.item_at Tree (Dir.fin_to_dir_vec i).reverse
+
+def tree_proof_at_fin {F: Type} {H: Hash F 2} (Tree : MerkleTree F H d) (i : Fin (2^d)): Vector F d :=
+  MerkleTree.proof Tree (Dir.fin_to_dir_vec i).reverse
+
+def tree_set_at_fin {F: Type} {H: Hash F 2} (Tree : MerkleTree F H d) (i : Fin (2^d)) (Item : F): MerkleTree F H d :=
+  MerkleTree.set Tree (Dir.fin_to_dir_vec i).reverse Item
+
 ------------
 
 def TreeInsert [Fact (perfect_hash poseidon₂)]
@@ -102,3 +141,104 @@ theorem insertion_round_uncps [Fact (perfect_hash poseidon₂)]
   rw [<-hzero]
   rw [proof_of_set]
   simp [MerkleTree.recover_proof_is_root]
+
+theorem insertion_round_prep_index_validation
+  (Root Index Item : F) (Proof : Vector F D) (k : F → Prop) :
+  insertion_round_prep Index Item Root Proof k → is_index_in_range D Index := by
+  unfold insertion_round_prep
+  intro ⟨out, prop, is_bin, _⟩
+  rw [recover_binary_zmod_bit is_bin] at prop
+  rw [←prop]
+  simp [is_index_in_range]
+  rw [binary_nat_zmod_equiv_mod_p]
+  apply Nat.lt_of_le_of_lt
+  apply Nat.mod_le
+  apply binary_nat_lt
+
+def TreeInsertPrep [Fact (perfect_hash poseidon₂)]
+  (Tree : MerkleTree F poseidon₂ D) (Index : F) (Item : F) (ix_small : is_index_in_range D Index) : MerkleTree F poseidon₂ D :=
+  let path := fin_to_bits_le ⟨Index.val, ix_small⟩
+  TreeInsert Tree (Vector.map Bit.toZMod path) Item
+
+theorem insertion_round_prep_uncps [Fact (perfect_hash poseidon₂)]
+  (Tree : MerkleTree F poseidon₂ D) (Index : F) (Item : F) (ix_small : is_index_in_range D Index) (k : F → Prop) :
+  let newTree := tree_set_at_fin Tree Index.val Item
+  let item := tree_item_at_fin newTree Index.val
+  let proof := tree_proof_at_fin newTree Index.val
+  tree_item_at_fin Tree Index = (0:F) →
+  (insertion_round_prep Index item Tree.root proof.reverse k ↔
+  k (TreeInsertPrep Tree Index Item ix_small).root) := by
+  simp
+  intro hzero
+  unfold insertion_round_prep
+  apply Iff.intro
+  . rintro ⟨ixbin, _⟩
+    casesm* (_ ∧ _)
+    rename_i h
+    rw [tree_item_at_fin, tree_proof_at_fin, tree_set_at_fin] at h
+    rw [ZMod.cast_eq_val] at h
+    rw [Dir.recover_binary_zmod'_to_dir (w := ixbin)] at h
+    . unfold TreeInsertPrep
+      rw [insertion_round_uncps] at h
+      rw [fin_to_bits_le_to_recover_binary_zmod' (v := Index) (w := ixbin)]
+      . simp
+        rw [vector_zmod_to_bit_to_zmod]
+        . simp [h]
+        . assumption
+      . simp [ix_small]
+      . assumption
+      . assumption
+      . assumption
+      . rw [tree_item_at_fin] at hzero
+        rw [ZMod.cast_eq_val] at hzero
+        rw [Dir.recover_binary_zmod'_to_dir (w := ixbin)] at hzero
+        . simp [hzero]
+        . rw [is_index_in_range] at ix_small
+          simp [ix_small]
+        . simp [Order]
+        . assumption
+        . assumption
+    . simp [is_index_in_range] at ix_small
+      simp [ix_small]
+    . simp [Order]
+    . assumption
+    . assumption
+  . intro h
+    simp [TreeInsertPrep] at h
+    let t : Vector F D := Vector.map Bit.toZMod (fin_to_bits_le ⟨Index.val, ix_small⟩)
+    refine ⟨t, ?_⟩
+    rw [tree_item_at_fin, tree_proof_at_fin, tree_set_at_fin]
+    rw [ZMod.cast_eq_val]
+    rw [Dir.recover_binary_zmod'_to_dir (w := t)]
+    rw [insertion_round_uncps]
+    simp
+    refine ⟨?_, ?_, ?_⟩
+    . rw [recover_binary_of_to_bits]
+      rw [fin_to_bits_le]
+      split
+      . assumption
+      . contradiction
+    . simp [vector_binary_of_bit_to_zmod]
+    . simp [h]
+    . rw [tree_item_at_fin] at hzero
+      rw [ZMod.cast_eq_val] at hzero
+      rw [Dir.recover_binary_zmod'_to_dir (w := t)] at hzero
+      . simp [hzero]
+      . rw [is_index_in_range] at ix_small
+        simp [ix_small]
+      . simp [Order]
+      . simp [vector_binary_of_bit_to_zmod]
+      . rw [recover_binary_of_to_bits]
+        simp [fin_to_bits_le]
+        split
+        . assumption
+        . contradiction
+    . rw [is_index_in_range] at ix_small
+      simp [ix_small]
+    . simp [Order]
+    . simp [vector_binary_of_bit_to_zmod]
+    . rw [recover_binary_of_to_bits]
+      simp [fin_to_bits_le]
+      split
+      . assumption
+      . contradiction
